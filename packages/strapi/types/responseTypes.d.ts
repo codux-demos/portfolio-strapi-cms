47985ad94dca169a831f8c57bf8997953ad4b046
alissawix <alissav@wix.com>
2023-11-21 18:11:23 +0200

    add other types

--- packages/strapi/types/responseTypes.d.ts
+++ packages/strapi/types/responseTypes.d.ts
@@ -1,2 +1,8 @@
 export * from '../src/api/todo/content-types/todo/todo';
+export * from '../src/api/about/content-types/about/about'
+export * from '../src/api/project/content-types/project/project';
+export * from '../src/api/project-item/content-types/project-item';
+
 export * from '../src/common/schemas-to-ts/Payload';
+export * from '../src/common/schemas-to-ts/Media';
+export * from '../src/common/schemas-to-ts/MediaFormat';

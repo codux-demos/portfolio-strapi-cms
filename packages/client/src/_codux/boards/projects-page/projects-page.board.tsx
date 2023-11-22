ee6083259dc4977b6eff4d03bc7337ef1dfcd610
alissawix <alissav@wix.com>
2023-11-22 14:42:23 +0200

    boards use routes config

--- packages/client/src/_codux/boards/projects-page/projects-page.board.tsx
+++ packages/client/src/_codux/boards/projects-page/projects-page.board.tsx
@@ -1,12 +1,13 @@
 import { createBoard } from '@wixc3/react-board';
 import { FakeDataRealRoutes } from '../../board-wrappers/fake-data-real-routes';
 import { ProjectsPage } from '../../../views/projects-page/projects-page';
+import { ROUTES } from '../../../router/config';
 
 export default createBoard({
   name: 'Projects Page',
   Board: () => (
     <FakeDataRealRoutes
-      path="/"
+      path={ROUTES.projects.to()}
       settings={{
         numberOfItems: 30,
       }}

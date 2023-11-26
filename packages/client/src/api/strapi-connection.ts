76b2318b325d8292c42a24cac0443bc968d597a3
alissawix <150237156+alissawix@users.noreply.github.com>
2023-11-26 08:46:42 +0200

    Projects accordion (#12)
    * add position sticky to the projects gallery
    
    * make the height of the project boxes responsive
    
    * add some docs

--- packages/client/src/api/strapi-connection.ts
+++ packages/client/src/api/strapi-connection.ts
@@ -6,6 +6,10 @@ function buildUrl(apiName: string, params?: Record<string, string>) {
 }
 
 const reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+/**
+ * this function is passed to the `JSON.parse`. It attempts to recognise date ISO strings and parse them into dates
+ * so they will be easier to work with.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function parseJsonDates(key: string, value: any): any {
   if (typeof value === 'string') {
@@ -14,6 +18,13 @@ function parseJsonDates(key: string, value: any): any {
   }
   return value;
 }
+
+/**
+ * wrapps the fetch to Strapi calls.
+ * we have this wrapper for 2 reasons:
+ * 1. so we don't have to duplicate code (like parsing strings to dates)
+ * 2. so we can easily mock the fetch and return fake data, or replace the fetch with anything else
+ */
 export class StrapiConnection implements Connection {
   async sendGetRequest<T>(apiPath: string[], params?: { [key: string]: string }) {
     try {
--- packages/client/src/components/projects-gallery/projects-gallery.tsx
+++ packages/client/src/components/projects-gallery/projects-gallery.tsx
@@ -1,8 +1,9 @@
-import { Link } from 'react-router-dom';
 import { useProjects } from '../../api/api-hooks';
 import styles from './projects-gallery.module.scss';
-import { ROUTES } from '../../router/config';
 import { getImageUrl } from '../../api/strapi-connection';
+import { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../router/config';
 
 export interface ProjectsGalleryProps {
   className?: string;
@@ -14,13 +15,37 @@ export interface ProjectsGalleryProps {
  */
 export const ProjectsGallery = ({ className }: ProjectsGalleryProps) => {
   const { data: projects } = useProjects();
+  const rootRef = useRef<HTMLDivElement>(null);
+
+  /**
+   * the idea behind this strange accordion is that each project description box has
+   * position sticky and top = bottom of the project description box before it.
+   *
+   * in addition the height of the project description boxes is dynamic so it will behave well
+   * on a smaller screen / more projects.
+   * otherwise if you have a lot of items the last ones aren't reachable
+   *
+   * so, since the top and height of the projects depends on the amount of the projects we have to do it
+   * with inlint style.
+   */
+  const boxHeight = `min(calc(100vh / ${projects?.data.length}), 4rem)`;
   return (
-    <div className={`${styles.root} ${className}`}>
-      {projects?.data.map((project) => (
-        <Link to={ROUTES.project.to(project.id)} key={project.id}>
-          <img src={getImageUrl(project.attributes.coverImage)} />
-        </Link>
-      ))}
+    <div className={`${styles.root} ${className}`} ref={rootRef}>
+      {projects?.data.map((project, index) => [
+        <Link
+          to={ROUTES.project.to(project.id)}
+          key={`link_${project.id}`}
+          className={styles.box}
+          style={{ top: `calc(${index} * ${boxHeight}`, height: boxHeight, position: 'sticky', minHeight: '1.5rem' }}
+        >
+          {project.attributes.title}
+        </Link>,
+        <img
+          key={`img_${project.id}`}
+          src={getImageUrl(project.attributes.coverImage)}
+          style={{ top: `calc(${index + 1} * ${boxHeight})`, position: 'sticky' }}
+        />,
+      ])}
     </div>
   );
 };

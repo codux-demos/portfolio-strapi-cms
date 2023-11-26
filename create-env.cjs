150197ab83b01bd8b0cb6a1ba16905a09c5a044e
Sagiv Dayan <sagiv.keeper@gmail.com>
2023-11-26 17:00:29 +0200

    modify script to accept port as first parameter

--- create-env.cjs
+++ create-env.cjs
@@ -1,15 +1,21 @@
 const crypto = require('crypto');
 const fs = require('fs');
+const { argv } = require('node:process');
 const os = require('os');
 const path = require('path');
 
+// taking care of the '.env' file related to the strapi package
 const envExampleValues = fs
   .readFileSync(path.join(__dirname, 'packages', 'strapi', '.env.example'))
   .toString()
   .split('\n');
 const envValues = envExampleValues
   .map((line) =>
-    line.includes('[change-me]') ? line.replaceAll('[change-me]', crypto.randomBytes(16).toString('base64')) : line,
+    line.includes('[change-me]')
+      ? line.replaceAll('[change-me]', crypto.randomBytes(16).toString('base64'))
+      : line.includes('[PORT]')
+        ? line.replaceAll('[PORT]', process.argv[2] || 5000)
+        : line,
   )
   .join('\n');
 
@@ -23,3 +29,16 @@ fs.writeFileSync(
   path.join(__dirname, 'packages', 'strapi', 'package.json'),
   JSON.stringify(pkgJsonObject, null, 2) + os.EOL,
 );
+
+// taking care of the '.env' file related to the client package
+{
+  const envExampleValues = fs
+    .readFileSync(path.join(__dirname, 'packages', 'client', '.env.example'))
+    .toString()
+    .split('\n');
+  const envValues = envExampleValues
+    .map((line) => (line.includes('[PORT]') ? line.replaceAll('[PORT]', argv[2] || 5000) : line))
+    .join('\n');
+
+  fs.writeFileSync(path.join(__dirname, 'packages', 'client', '.env'), envValues);
+}

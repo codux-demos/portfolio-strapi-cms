4eeab104fce1078dab0b39ed1652ad25ccfbcbce
alissawix <150237156+alissawix@users.noreply.github.com>
2023-11-26 09:19:23 +0200

    Nav menu (#13)
    * delete global css
    
    * create a menu with radix
    
    * add floating-ui for the  sub menu
    
    * add reset css and some fixes
    
    * Update README.md

--- packages/client/src/components/header/header.tsx
+++ packages/client/src/components/header/header.tsx
@@ -1,13 +1,10 @@
-import { NavLink } from 'react-router-dom';
-import { ROUTES } from '../../router/config';
 import styles from './header.module.scss';
+import { SiteMenu } from '../site-menu/site-menu';
 
 export interface HeaderProps {
   className?: string;
 }
 
-const navLinkClass = ({ isActive }: { isActive: boolean }) =>
-  [styles['nav-link'], isActive ? styles.active : ''].join(' ');
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
@@ -16,14 +13,7 @@ export const Header = ({ className }: HeaderProps) => {
   return (
     <div className={`${styles.root} ${className}`}>
       Header Stuff
-      <div>
-        <NavLink to={ROUTES.projects.to()} end className={navLinkClass}>
-          Home
-        </NavLink>
-        <NavLink to="/non-existant" end className={navLinkClass}>
-          Other
-        </NavLink>
-      </div>
+      <SiteMenu />
     </div>
   );
 };

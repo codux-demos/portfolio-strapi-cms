333b1d340c882fcc645bde28ccee8459eb6fcd3a
Sagiv Dayan <sagiv.keeper@gmail.com>
2023-11-26 12:41:09 +0200

    make board not a snippet

--- packages/client/src/_codux/boards/about-page/about-page.board.tsx
+++ packages/client/src/_codux/boards/about-page/about-page.board.tsx
@@ -2,7 +2,6 @@ import { createBoard } from '@wixc3/react-board';
 import { AboutPage } from '../../../views/about-page/about-page';
 
 export default createBoard({
-    name: 'About Page',
-    Board: () => <AboutPage />,
-    isSnippet: true,
+  name: 'About Page',
+  Board: () => <AboutPage />,
 });

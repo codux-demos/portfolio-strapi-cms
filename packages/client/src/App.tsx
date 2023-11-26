474eecd3a003bc105d6d114ea415c6472e5557f6
alissawix <alissav@wix.com>
2023-11-26 09:58:07 +0200

    test commit

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,6 +3,7 @@ import { APIContextProvider } from './api';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './router/routes';
 
+//bogus comment
 function App() {
   const router = useMemo(() => createBrowserRouter(routes), []);
   return (

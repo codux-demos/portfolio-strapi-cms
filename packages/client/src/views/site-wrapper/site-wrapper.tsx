a45d3466be4b4f8bfad600cc43473cd0b6d8c940
alissawix <alissav@wix.com>
2023-11-27 07:17:34 +0200

    restore scroll

--- packages/client/src/views/site-wrapper/site-wrapper.tsx
+++ packages/client/src/views/site-wrapper/site-wrapper.tsx
@@ -2,13 +2,15 @@ import { Outlet } from 'react-router';
 import { Footer } from '../../components/footer/footer';
 import { Header } from '../../components/header/header';
 import SiteWrapper_module from './site-wrapper.module.scss';
+import { ScrollRestoration } from 'react-router-dom';
 
 export function SiteWrapper() {
-    return (
-        <div className={SiteWrapper_module.root}>
-            <Header />
-            <Outlet />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className={SiteWrapper_module.root}>
+      <ScrollRestoration />
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }

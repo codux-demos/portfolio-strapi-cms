f3f81ca61bc41a2c9a501961aaf29220770f24a4
alissawix <alissav@wix.com>
2023-11-29 15:17:34 +0200

    remove project-page

--- packages/client/src/router/routes.tsx
+++ packages/client/src/router/routes.tsx
@@ -1,7 +1,6 @@
 import { Navigate, RouteObject } from 'react-router-dom';
 import { SiteWrapper } from '../views/site-wrapper/site-wrapper';
 import { ProjectsPage } from '../views/projects-page/projects-page';
-import { ProjectPage } from '../views/project-page/project-page';
 import { AboutPage } from '../views/about-page/about-page';
 import { ROUTES } from './config';
 import { ItemsPage } from '../views/items-page/items-page';

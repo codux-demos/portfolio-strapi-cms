82e6dc5a9a0c5e883e6eab9d13db1f643fae49fd
alissawix <alissav@wix.com>
2023-11-30 09:24:41 +0200

    styling of wrapper home and about

--- packages/client/src/views/about-page/about-page.tsx
+++ packages/client/src/views/about-page/about-page.tsx
@@ -3,36 +3,33 @@ import { getImageUrl } from '../../api/strapi-connection';
 import styles from './about-page.module.scss';
 import Markdown from 'markdown-to-jsx';
 
-export interface AboutPageProps {
-    className?: string;
-}
-
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
-export const AboutPage = ({ className }: AboutPageProps) => {
-    const { data: aboutItem } = apiHooks.useAbout();
+export const AboutPage = () => {
+  const { data: aboutItem } = apiHooks.useAbout();
 
-    if (!aboutItem) return null;
+  if (!aboutItem) return null;
 
-    return (
-        <div className={`${styles.root} ${className}`}>
-            <div>
-                <strong>Title:</strong>
-                <span>{aboutItem.data.attributes.title}</span>
-            </div>
-            <div>
-                <strong>Subtitle:</strong>
-                <span>{aboutItem.data.attributes.subTitle}</span>
-            </div>
-            <div>
-                <strong>Image:</strong>
-                {aboutItem.data.attributes.image && <img src={getImageUrl(aboutItem?.data.attributes.image)} />}
-            </div>
-            <div className="markdown">
-                <Markdown>{aboutItem.data.attributes.richtext || ''}</Markdown>
-            </div>
+  return (
+    <>
+      <div className={`${styles.rectangle} ${styles.text}`}>
+        <div>
+          <strong>Title:</strong>
+          <span>{aboutItem.data.attributes.title}</span>
         </div>
-    );
+        <div>
+          <strong>Subtitle:</strong>
+          <span>{aboutItem.data.attributes.subTitle}</span>
+        </div>
+        <div className="markdown">
+          <Markdown>{aboutItem.data.attributes.richtext || ''}</Markdown>
+        </div>
+      </div>
+      <div className={styles.rectangle}>
+        {aboutItem.data.attributes.image && <img src={getImageUrl(aboutItem?.data.attributes.image)} />}
+      </div>
+    </>
+  );
 };
--- packages/client/src/views/projects-page/projects-page.tsx
+++ packages/client/src/views/projects-page/projects-page.tsx
@@ -3,17 +3,21 @@ import { ProjectsGallery } from '../../components/projects-gallery/projects-gall
 import * as theme from '../../styles/theme.module.scss';
 
 export interface ItemsProps {
-  className?: string;
+    className?: string;
 }
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const ProjectsPage = ({ className }: ItemsProps) => {
-  return (
-    <div className={`${styles.root} ${className}`}>
-      <h3>some static description of my portfolio</h3>
-      <ProjectsGallery headerHeight={theme.headerHeight} />
-    </div>
-  );
+    return (
+        <div className={`${styles.root} ${className}`}>
+            <div className={`${styles.rectangle} ${styles.text}`}>
+                <h3 className={styles.title}>some static description of my portfolio</h3>
+            </div>
+            <div className={`${styles.rectangle} ${styles.img}`}></div>
+
+            <ProjectsGallery className={styles.gallery} headerHeight={theme.headerHeight} />
+        </div>
+    );
 };

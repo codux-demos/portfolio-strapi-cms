a043de6c52e68813ab0b813e60bf1548b7d4e535
alissawix <alissav@wix.com>
2023-11-30 10:24:49 +0200

    start animation

--- packages/client/src/components/header/header.tsx
+++ packages/client/src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import styles from './header.module.scss';
 import { SiteMenu } from '../site-menu/site-menu';
+import { useScroll, motion } from 'framer-motion';
 
 export interface HeaderProps {
   className?: string;
@@ -10,13 +11,15 @@ export interface HeaderProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const Header = ({ className }: HeaderProps) => {
+  const { scrollYProgress } = useScroll();
+
   return (
-    <div className={`${styles.root} ${className}`}>
+    <motion.div className={`${styles.root} ${className}`} style={{ opacity: scrollYProgress }}>
       <div className={styles.leftContainer}>
         <span className={styles.logo}>NAME PORT</span>
         <SiteMenu />
       </div>
       <a href="#footer">Contant</a>
-    </div>
+    </motion.div>
   );
 };

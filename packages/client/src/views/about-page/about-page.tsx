797e41bfaeac563d26565e111fd9dd688973c235
alissawix <alissav@wix.com>
2023-11-30 09:48:42 +0200

    add fake markdown

--- packages/client/src/views/about-page/about-page.tsx
+++ packages/client/src/views/about-page/about-page.tsx
@@ -8,28 +8,22 @@ import Markdown from 'markdown-to-jsx';
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const AboutPage = () => {
-  const { data: aboutItem } = apiHooks.useAbout();
+    const { data: aboutItem } = apiHooks.useAbout();
 
-  if (!aboutItem) return null;
+    if (!aboutItem) return null;
 
-  return (
-    <>
-      <div className={`${styles.rectangle} ${styles.text}`}>
-        <div>
-          <strong>Title:</strong>
-          <span>{aboutItem.data.attributes.title}</span>
-        </div>
-        <div>
-          <strong>Subtitle:</strong>
-          <span>{aboutItem.data.attributes.subTitle}</span>
-        </div>
-        <div className="markdown">
-          <Markdown>{aboutItem.data.attributes.richtext || ''}</Markdown>
-        </div>
-      </div>
-      <div className={styles.rectangle}>
-        {aboutItem.data.attributes.image && <img src={getImageUrl(aboutItem?.data.attributes.image)} />}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={`${styles.rectangle} ${styles.text}`}>
+                <h3 className={styles.title}>{aboutItem.data.attributes.title}</h3>
+                <h5 className={styles.sub}>{aboutItem.data.attributes.subTitle}</h5>
+                <div className="markdown">
+                    <Markdown>{aboutItem.data.attributes.richtext || ''}</Markdown>
+                </div>
+            </div>
+            <div className={styles.rectangle}>
+                {aboutItem.data.attributes.image && <img src={getImageUrl(aboutItem?.data.attributes.image)} />}
+            </div>
+        </>
+    );
 };

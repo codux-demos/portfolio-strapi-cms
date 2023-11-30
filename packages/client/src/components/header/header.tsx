9d00a421ca647c53e93681e640b9fc7793780b39
alissawix <alissav@wix.com>
2023-11-30 10:49:17 +0200

    change header bg opacity

--- packages/client/src/components/header/header.tsx
+++ packages/client/src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import styles from './header.module.scss';
 import { SiteMenu } from '../site-menu/site-menu';
-import { useScroll, motion } from 'framer-motion';
+import { useScroll, motion, useMotionTemplate, useMotionValue, useTransform } from 'framer-motion';
+import * as theme from '../../styles/theme.module.scss';
 
 export interface HeaderProps {
   className?: string;
@@ -11,10 +12,13 @@ export interface HeaderProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const Header = ({ className }: HeaderProps) => {
-  const { scrollYProgress } = useScroll();
+  const { scrollY } = useScroll();
+  const headerHeight = useMotionValue(parseInt(theme.headerHeight));
+  const opacity = useTransform(() => scrollY.get() / headerHeight.get());
+  const bg = useMotionTemplate`rgba(255, 255, 255, ${opacity})`;
 
   return (
-    <motion.div className={`${styles.root} ${className}`} style={{ opacity: scrollYProgress }}>
+    <motion.div className={`${styles.root} ${className}`} style={{ background: bg }}>
       <div className={styles.leftContainer}>
         <span className={styles.logo}>NAME PORT</span>
         <SiteMenu />

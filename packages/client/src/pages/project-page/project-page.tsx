6b6ba59ff25bd35247b61fe1bcfd430630136096
alissawix <alissav@wix.com>
2023-12-03 15:33:47 +0200

    add back to top

--- packages/client/src/pages/project-page/project-page.tsx
+++ packages/client/src/pages/project-page/project-page.tsx
@@ -3,10 +3,10 @@ import styles from './project-page.module.scss';
 import { RouteParams } from '../../router/config';
 import { apiHooks } from '../../api';
 import { ProjectItem } from '../../components/project-item/project-item';
-import cx from 'classnames'
+import cx from 'classnames';
 
 export interface ProjectPage {
-  className?: string;
+    className?: string;
 }
 
 /**
@@ -14,35 +14,38 @@ export interface ProjectPage {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const ProjectPage = ({ className }: ProjectPage) => {
-  const { id: strId } = useParams<RouteParams['/projects/:id']>();
-  const id = strId ? parseInt(strId) : -1;
+    const { id: strId } = useParams<RouteParams['/projects/:id']>();
+    const id = strId ? parseInt(strId) : -1;
 
-  const { data } = apiHooks.useProject(id);
-  const { data: projectItems } = apiHooks.useProjectItems(id);
+    const { data } = apiHooks.useProject(id);
+    const { data: projectItems } = apiHooks.useProjectItems(id);
 
-  const [firstItem, ...restItems] = projectItems?.data || [];
-  if (!projectItems) {
-    return <div>Loading...</div>;
-  }
-  if (!firstItem) {
-    return <div>there are no items in this project</div>;
-  }
-  return (
-    <div className={cx(styles.root, className)}>
-      <div className={styles.gallery}>
-        <div key="first" className={styles.galleryItem}>
-          <ProjectItem projectItemAttr={firstItem?.attributes} />
+    const [firstItem, ...restItems] = projectItems?.data || [];
+    if (!projectItems) {
+        return <div>Loading...</div>;
+    }
+    if (!firstItem) {
+        return <div>there are no items in this project</div>;
+    }
+    return (
+        <div id="top" className={cx(styles.root, className)}>
+            <div className={styles.gallery}>
+                <div key="first" className={styles.galleryItem}>
+                    <ProjectItem projectItemAttr={firstItem?.attributes} />
+                </div>
+                <div key="desc" className={styles.galleryItem} style={{ maxWidth: '100%' }}>
+                    <h3 className={styles.title}>{data?.attributes.title}</h3>
+                    <p>{data?.attributes.description}</p>
+                </div>
+                {restItems.map((item) => (
+                    <div key={item.id} className={styles.galleryItem}>
+                        <ProjectItem projectItemAttr={item.attributes} />
+                    </div>
+                ))}
+            </div>
+            <div>
+                <a href="#top" className={styles.backToTop}>Back to top</a>
+            </div>
         </div>
-        <div key="desc" className={styles.galleryItem} style={{ maxWidth: '100%' }}>
-          <h3 className={styles.title}>{data?.attributes.title}</h3>
-          <p>{data?.attributes.description}</p>
-        </div>
-        {restItems.map((item) => (
-          <div key={item.id} className={styles.galleryItem}>
-            <ProjectItem projectItemAttr={item.attributes} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    );
 };

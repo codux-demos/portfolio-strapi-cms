c45c6d666b99b6d69d44ae40038a4fd19651d475
Sagiv Dayan <67858097+TheAlmightyCrumb@users.noreply.github.com>
2023-12-04 09:51:13 +0200

    wip: moving text animation (#48)
    * initial moving text
    
    * add top and bottom borderlines
    
    * use theme
    
    ---------
    
    Co-authored-by: alissawix <alissav@wix.com>

--- packages/client/src/pages/home-page/home-page.tsx
+++ packages/client/src/pages/home-page/home-page.tsx
@@ -2,23 +2,41 @@ import styles from './home-page.module.scss';
 import { ProjectsGallery } from '../../components/projects-gallery/projects-gallery';
 import * as theme from '../../styles/theme.module.scss';
 import cx from 'classnames';
+import { motion } from 'framer-motion';
 export interface HomePageProps {
-    className?: string;
+  className?: string;
 }
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const HomePage = ({ className }: HomePageProps) => {
-    return (
-        <div className={cx(styles.root, className)}>
-            <h3 className={cx(styles.rectangle, styles.text)}>some static description of my portfolio</h3>
-            <div className={cx(styles.rectangle, styles.img)}></div>
-            <div className={styles.logoWrapper}>
-                <h1 className={styles.logo}>NAME PORT</h1>
-            </div>
+  const marqueeVariants = {
+    animate: {
+      x: [0, -1000],
+      transition: {
+        x: {
+          repeat: Infinity,
+          duration: 8,
+          ease: 'linear',
+        },
+      },
+    },
+  };
 
-            <ProjectsGallery className={styles.gallery} headerHeight={theme.headerHeight} />
-        </div>
-    );
+  return (
+    <div className={cx(styles.root, className)}>
+      <h3 className={cx(styles.rectangle, styles.text)}>some static description of my portfolio</h3>
+      <div className={cx(styles.rectangle, styles.img)}></div>
+      <div className={styles.logoWrapper}>
+        <h1 className={styles.logo}>NAME PORT</h1>
+      </div>
+      <div className={styles.marquee}>
+        <motion.div className={styles.marqueeContent} variants={marqueeVariants} animate="animate">
+          {Array(2).fill('Describing the business and the portfolio in a short description. ')}
+        </motion.div>
+      </div>
+      <ProjectsGallery className={styles.gallery} headerHeight={theme.headerHeight} />
+    </div>
+  );
 };

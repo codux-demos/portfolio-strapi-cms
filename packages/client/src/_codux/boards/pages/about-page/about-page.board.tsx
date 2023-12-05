e035fd7f1b7369fe4387aebd344e0c59e6e6c25b
alissawix <alissav@wix.com>
2023-12-05 12:33:29 +0200

    about page

--- packages/client/src/_codux/boards/pages/about-page/about-page.board.tsx
+++ packages/client/src/_codux/boards/pages/about-page/about-page.board.tsx
@@ -4,15 +4,13 @@ import { AboutPage } from '../../../../pages/about-page/about-page';
 import { PageWrapper } from '../../../board-wrappers/page-wrapper';
 
 export default createBoard({
-  name: 'About Page',
-  Board: () => (
-    <PageWrapper path={ROUTES.about.to()}>
-      <AboutPage />
-    </PageWrapper>
-  ),
-  environmentProps: {
-    windowWidth: 1024,
-    windowHeight: 768,
-    canvasWidth: 1037,
-  },
+    name: 'About Page',
+    Board: () => (
+        <PageWrapper path={ROUTES.about.to()}>
+            <AboutPage />
+        </PageWrapper>
+    ),
+    environmentProps: {
+        windowWidth: 375, canvasWidth: 1037, windowHeight: 667
+    },
 });

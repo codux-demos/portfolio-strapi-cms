083225c1b63014ee70630829e0909eb4f0b93d89
alissawix <alissav@wix.com>
2023-12-05 13:26:48 +0200

    home page

--- packages/client/src/_codux/boards/pages/home-page/home-page.board.tsx
+++ packages/client/src/_codux/boards/pages/home-page/home-page.board.tsx
@@ -4,19 +4,21 @@ import { HomePage } from '../../../../pages/home-page/home-page';
 import { ROUTES } from '../../../../router/config';
 
 export default createBoard({
-  name: 'Home Page',
-  Board: () => (
-    <PageWrapper
-      path={ROUTES.projects.to()}
-      settings={{
-        numberOfItems: 4,
-      }}
-    >
-      <HomePage />
-    </PageWrapper>
-  ),
-  isSnippet: false,
-  environmentProps: {
-    canvasWidth: 1046,
-  },
+    name: 'Home Page',
+    Board: () => (
+        <PageWrapper
+            path={ROUTES.projects.to()}
+            settings={{
+                numberOfItems: 4,
+            }}
+        >
+            <HomePage />
+        </PageWrapper>
+    ),
+    isSnippet: false,
+    environmentProps: {
+        canvasWidth: 1046,
+        windowWidth: 414,
+        windowHeight: 896
+    },
 });

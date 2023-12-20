7f93e06ed660ed4e5cf67a50a9eef79f5757d0c9
ShireeWill <shiree@wix.com>
2023-12-20 11:07:04 +0200

    fixing fonts sizes

--- packages/client/src/components/footer/footer.tsx
+++ packages/client/src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.scss';
 import cx from 'classnames';
 
 export interface FooterProps {
-  className?: string;
+    className?: string;
 }
 
 /**
@@ -11,27 +11,27 @@ export interface FooterProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const Footer = ({ className }: FooterProps) => {
-  const { data } = apiHooks.useAbout();
+    const { data } = apiHooks.useAbout();
 
-  return (
-    <div id="footer" className={cx(styles.root, className)}>
-      <div>
-        <a href={`mailto:${data?.data.attributes.email}`}>{data?.data.attributes.email}</a>
-        <br />
-        <span>{data?.data.attributes.phone}</span>
-      </div>
-      <div className={styles.social}>
-        <a href={data?.data.attributes.instagram || '/'} target="_blank" rel="noreferrer">
-          Instagram
-        </a>
-        <a href={data?.data.attributes.facebook || '/'} target="_blank" rel="noreferrer">
-          Facebook
-        </a>
-        <a href={data?.data.attributes.pinterest || '/'} target="_blank" rel="noreferrer">
-          Pinterest
-        </a>
-      </div>
-      <span className={styles.copyright}>© 2020 Career Karma and some more</span>
-    </div>
-  );
+    return (
+        <div id="footer" className={cx(styles.root, className)}>
+            <div className={styles.social}>
+                <a href={`mailto:${data?.data.attributes.email}`}>{data?.data.attributes.email}</a>
+                <br />
+                <span>{data?.data.attributes.phone}</span>
+            </div>
+            <div className={styles.social}>
+                <a href={data?.data.attributes.instagram || '/'} target="_blank" rel="noreferrer">
+                    Instagram
+                </a>
+                <a href={data?.data.attributes.facebook || '/'} target="_blank" rel="noreferrer">
+                    Facebook
+                </a>
+                <a href={data?.data.attributes.pinterest || '/'} target="_blank" rel="noreferrer">
+                    Pinterest
+                </a>
+            </div>
+            <span className={styles.copyright}>© 2020 Career Karma and some more</span>
+        </div>
+    );
 };
